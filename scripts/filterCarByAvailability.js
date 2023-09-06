294471f4ca90f210be699d99131f952f7e36dbbe
mnuralim <shadownur345.com>
2023-09-06 18:56:26 +0800

    first commit

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -7,14 +7,14 @@ function filterCarByAvailability(cars) {
 
   // Tulis code-mu disini
   // 1) kita melakukan looping untuk check 1 1 data dalam array
-  for(let i = 0; i < cars.length; i++) {
+  for (let i = 0; i < cars.length; i++) {
     // 2) di check data yang available true
     // console.log(`ini data index ke : ${i + 1}`)
     // console.log('====================')
-    if(cars[i].available === true) {
-      console.log(cars[i].available)
+    if (cars[i].available === true) {
+      console.log(cars[i].available);
       // 3. masukkan data yang sudah di filter
-      result.push(cars[i])
+      result.push(cars[i]);
     }
   }
 
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -7,7 +7,23 @@ function sortCarByYearDescendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  //deklarasi panjang data array(cars)
+  const len = result.length;
 
+  //loop untuk mengatur jumlah iterasi
+  for (let i = 0; i < len - 1; i++) {
+    //loop untuk mengatur pertukaran posisi atau swap
+    for (let j = 0; j < len - 1 - i; j++) {
+      //bandingkan element saat ini dengan element setelahnya
+      if (result[j].year < result[j + 1].year) {
+        //jika elemen saat ini lebih besar dari pada element setelahnya,maka posisi ditukar
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+      console.log(result);
+    }
+  }
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
 }
